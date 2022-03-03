9fd5727fbd4c02f63c53c9b9fffb1bbf14dd3ef2
slipgrass <histo-ann@yandex.ru>
2022-03-04 00:52:17 +0500

    дз 1 занятие

---
+++ #1.py
@@ -0,0 +1,11 @@
+#Поработайте с переменными, создайте несколько, выведите на экран.
+#Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
+n = "cell"
+print(n)
+print(type(n))
+name = input("Enter your name: ")
+exam_date = input("Enter the date of your exam: ")
+exam_mark = input("Enter your exam mark: ")
+print(f"Hello, {name}, your exam was on {exam_date}, your mark is {exam_mark}. "
+      f"\nYou will be informed about "
+      f"\nthe date of your second attempt.")
